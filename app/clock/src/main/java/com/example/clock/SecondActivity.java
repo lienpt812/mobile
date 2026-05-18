4600d35502380b2637e0fa62a014708c0590c134
lienpt812 <phamthilien08122005@gmail.com>
2026-05-18 13:46:04 +0700

    update

--- app/clock/src/main/java/com/example/clock/SecondActivity.java
+++ app/clock/src/main/java/com/example/clock/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.clock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(view -> finish());
     }
 }
